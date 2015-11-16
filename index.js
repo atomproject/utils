eeb3dc9c0e519ae23f26d62aabeed6645c2bfa47
Ajinkya Tejankar <tejankarajinkya@gmail.com>
2015-11-16 16:05:09 +0530

    Observer complete

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 var chokidar = require('chokidar');
+var exec = require('child_process').exec;
 var path = require('path');
 var fs = require('fs');
 var split = require('split');
 var files = [];
+var pathsMap = {};
 var lines = fs.createReadStream('paths.txt')
 	.pipe(split());
 
+function strip(str) {
+	return str.replace(/^\s*/, '').replace(/\s*$/, '');
+}
+
 lines.on('data', function(line) {
+	var parts, dest, src;
 	if (line) {
-		files.push(path.resolve(line));
+		var parts = line.split('=>');
+		src = path.resolve(strip(parts[0]));
+		dest = path.resolve(strip(parts[1]));
+
+		files.push(src);
+		pathsMap[src] = dest;
 	}
 });
 
-lines.on('end', watchFiles);
-// lines.on('end', function() {
-// 	files.forEach(function(file) {
-// 		console.log(file);
-// 	})
-// });
+// lines.on('end', watchFiles);
+lines.on('end', function() {
+	files.forEach(function(file) {
+		console.log(`${file} => ${pathsMap[file]}`);
+	})
+});
 
 function watchFiles() {
 	var watcher = chokidar.watch(files);
@@ -26,7 +38,12 @@ function watchFiles() {
 }
 
 function onFileChange(path) {
+	var src = fs.createReadStream(path);
+	var dest = fs.createWriteStream(pathsMap[path].dest);
+
 	//if path contains bower.json then run bower update
 	//in the output dir
 	console.log(`changed: ${path}`);
+
+	src.pipe(dest);
 }
